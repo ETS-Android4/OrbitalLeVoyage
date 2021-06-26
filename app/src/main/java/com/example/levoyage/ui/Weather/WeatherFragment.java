9161f5fd4a1c7c708246fe2d551891f99bec5a69
tsy24 <szeying3@gmail.com>
2021-06-27 00:53:19 +0800

    Debug weather fragment

--- app/src/main/java/com/example/levoyage/ui/Weather/WeatherFragment.java
+++ app/src/main/java/com/example/levoyage/ui/Weather/WeatherFragment.java
@@ -8,25 +8,32 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import com.android.volley.Header;
 import com.example.levoyage.R;
+import com.google.firebase.database.annotations.NotNull;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONObject;
 
-public class WeatherFragment extends AppCompatActivity {
+public class WeatherFragment extends Fragment {
 
     final String API = "798183b24b356e657baaff7ec8bfc4c6";
     final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
@@ -45,45 +52,42 @@ public class WeatherFragment extends AppCompatActivity {
     LocationManager locationManager;
     LocationListener locationListener;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_weather);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_weather, container, false);
+    }
 
-        weatherState = findViewById(R.id.weather_condition);
-        temperature = findViewById(R.id.temperature);
-        weatherIcon = findViewById(R.id.weather_icon);
-        cityFinder = findViewById(R.id.changeCityButton);
-        cityName = findViewById(R.id.city_name);
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        weatherState = view.findViewById(R.id.weather_condition);
+        temperature = view.findViewById(R.id.temperature);
+        weatherIcon = view.findViewById(R.id.weather_icon);
+        cityFinder = view.findViewById(R.id.changeCityButton);
+        cityName = view.findViewById(R.id.city_name);
+
+        getWeatherForCurrentLocation();
 
         cityFinder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(WeatherFragment.this, cityFinder.class);
-                startActivity(intent);
+//                Navigation.findNavController(v).navigate();
             }
         });
     }
 
 //    @Override
-//    protected void onResume() {
+//    public void onResume() {
 //        super.onResume();
-//        getWeatherForCurrentLocation();
+//
+//        Intent intent = getIntent();
+//        String cityName = intent.getStringExtra("City");
+//        if (cityName != null) {
+//            getWeatherForNewCity(cityName);
+//        } else {
+//            getWeatherForCurrentLocation();
+//        }
 //    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Intent intent = getIntent();
-        String cityName = intent.getStringExtra("City");
-        if (cityName != null) {
-            getWeatherForNewCity(cityName);
-        } else {
-            getWeatherForCurrentLocation();
-        }
-    }
 
     private void getWeatherForNewCity(String city) {
         RequestParams params = new RequestParams();
@@ -93,13 +97,13 @@ public class WeatherFragment extends AppCompatActivity {
     }
 
     private void getWeatherForCurrentLocation() {
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(@NonNull Location location) {
                 String latitude = String.valueOf(location.getLatitude());
                 String longitude = String.valueOf(location.getLongitude());
-
+                Log.d("Track", "LocationChanged");
                 RequestParams params = new RequestParams();
                 params.put("lat", latitude);
                 params.put("lon", longitude);
@@ -122,7 +126,7 @@ public class WeatherFragment extends AppCompatActivity {
                 // unable to get location
             }
         };
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
@@ -130,7 +134,7 @@ public class WeatherFragment extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
             return;
         }
         locationManager.requestLocationUpdates(Location_Provider, MIN_TIME, MIN_DISTANCE, locationListener);
@@ -142,28 +146,26 @@ public class WeatherFragment extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         if (requestCode == REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(WeatherFragment.this, "Location Get Successfully", Toast.LENGTH_SHORT).show();
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(getContext(), "Location Get Successfully", Toast.LENGTH_SHORT).show();
 
             }
-        } else {
-            //Permission denied for users
         }
     }
 
     private void doNetworking(RequestParams params) {
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
+
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                Toast.makeText(WeatherFragment.this, "Data Get Success", Toast.LENGTH_SHORT).show();
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+                Toast.makeText(getContext(), "Data Get Success", Toast.LENGTH_SHORT).show();
 
                 WeatherData weatherD = WeatherData.fromJson(response);
                 updateUI(weatherD);
-//                super.onSuccess(statusCode, headers, response);
             }
 
-            @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
 //                super.onFailure(statusCode, headers, throwable, errorResponse);
             }
@@ -174,12 +176,12 @@ public class WeatherFragment extends AppCompatActivity {
         temperature.setText(weatherData.getTemperature());
         cityName.setText(weatherData.getCity());
         weatherState.setText(weatherData.getWeatherType());
-        int resourceID = getResources().getIdentifier(weatherData.getIcon(), "drawable", getPackageName());
+        int resourceID = getResources().getIdentifier(weatherData.getIcon(), "drawable", getActivity().getPackageName());
         weatherIcon.setImageResource(resourceID);
     }
 
     @Override
-    protected void onPause() {
+    public void onPause() {
         super.onPause();
         if(locationManager != null) {
             locationManager.removeUpdates(locationListener);
