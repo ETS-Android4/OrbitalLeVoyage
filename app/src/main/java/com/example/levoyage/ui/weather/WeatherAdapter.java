4407ecf90eae8325b82bc69256a33edd5564160e
tsy24 <szeying3@gmail.com>
2021-07-20 00:35:08 +0800

    Debug weather feature

--- app/src/main/java/com/example/levoyage/ui/weather/WeatherAdapter.java
+++ app/src/main/java/com/example/levoyage/ui/weather/WeatherAdapter.java
@@ -1,6 +1,7 @@
 package com.example.levoyage.ui.weather;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,57 +12,62 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.levoyage.R;
+import com.example.levoyage.ui.notes.NoteItem;
+import com.example.levoyage.ui.notes.NotesAdapter;
+import com.google.firebase.database.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-public class WeatherAdapter extends ArrayAdapter<String> {
+public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder> {
 
-    private Context mContext;
-    private Date[] dates;
-    private String[] temperature;
-    private String[] weatherIcons;
+    Context context;
+    List<WeatherItem> list;
 
-    public WeatherAdapter(@NonNull Context context, String[] weatherIcons, Date[] dates, String[] temperature) {
-        super(context, R.layout.weather_listview_item);
-        this.dates = dates;
-        this.temperature = temperature;
-        this.mContext = context;
-        this.weatherIcons = weatherIcons;
-    }
-
-    @Override
-    public int getCount() {
-        return super.getCount();
-    }
-
-
-    @NonNull
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        ViewHolder mViewHolder = new ViewHolder();
-        if (convertView == null) {
-            LayoutInflater mInflater = (LayoutInflater) mContext.
-                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            convertView = mInflater.inflate(R.layout.weather_listview_item, parent, false);
-            mViewHolder.mWeatherIcon = (ImageView) convertView.findViewById(R.id.weather_icon_lv);
-            mViewHolder.temp = (TextView) convertView.findViewById(R.id.temperature_lv);
-            mViewHolder.date = (TextView) convertView.findViewById(R.id.date_lv);
-            convertView.setTag(mViewHolder);
-        } else {
-            mViewHolder = (ViewHolder) convertView.getTag();
+    public WeatherAdapter(Context context, List<WeatherItem> list) {
+        this.context = context;
+        this.list = list;
         }
 
-//        int resourceID = getResources().getIdentifier(weatherIconsA[0], "drawable", getPackageName()); //TODO
-//        mViewHolder.mWeatherIcon.setImageResource(resourceID);
-        mViewHolder.temp.setText(temperature[position]);
-        mViewHolder.date.setText(dates[position].toString());
-        return convertView;
+    public static class WeatherViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView iconView;
+        TextView tempView, dateView;
+
+        public WeatherViewHolder(View itemView) {
+            super(itemView);
+            iconView = itemView.findViewById(R.id.weather_icon_lv);
+            tempView = itemView.findViewById(R.id.temperature_lv);
+            dateView = itemView.findViewById(R.id.date_lv);
+        }
     }
 
-    static class ViewHolder {
-        ImageView mWeatherIcon;
-        TextView temp, date;
+    @NonNull
+    @NotNull
+    @Override
+    public WeatherAdapter.WeatherViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.weather_listview_item, parent,false);
+        return new WeatherAdapter.WeatherViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(WeatherAdapter.WeatherViewHolder holder, int position) {
+        WeatherItem item = list.get(position);
+        holder.tempView.setText(String.format("%s°C", item.getTemperature()));
+        holder.dateView.setText(item.getDate());
+        int resourceID = context.getResources().getIdentifier(item.getIcon(), "drawable", context.getPackageName());
+        holder.iconView.setImageResource(resourceID);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
     }
 }
--- app/src/main/java/com/example/levoyage/ui/weather/WeatherFragment.java
+++ app/src/main/java/com/example/levoyage/ui/weather/WeatherFragment.java
@@ -9,6 +9,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,9 +17,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,18 +25,22 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-
-import com.android.volley.Header;
 import com.example.levoyage.R;
 import com.google.firebase.database.annotations.NotNull;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class WeatherFragment extends Fragment {
 
@@ -45,27 +48,19 @@ public class WeatherFragment extends Fragment {
     final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
     final String WEATHER_URL_5_DAYS = "https://api.openweathermap.org/data/2.5/onecall";
 
-
     final long MIN_TIME = 5000;
     final float MIN_DISTANCE = 1000;
     final int REQUEST_CODE = 101;
 
-    private Date[] datesA;
-    private String[] temperatureA;
-    private String[] weatherIconsA;
-
     String Location_Provider = LocationManager.GPS_PROVIDER;
 
-    TextView cityName, weatherState, temperature;
-    ImageView weatherIcon;
-    ListView weather5Days;
-
-    ProgressBar progressBar;
+    private TextView cityName, weatherState, temperature;
+    private ImageView weatherIcon;
+    private RecyclerView weather5Days;
+    private ProgressBar progressBar;
     private AlertDialog.Builder dialogBuilder;
     private AlertDialog dialog;
-
-    Button cityFinder;
-
+    private Button cityFinder;
     LocationManager locationManager;
     LocationListener locationListener;
 
@@ -87,40 +82,39 @@ public class WeatherFragment extends Fragment {
 
         getWeatherForCurrentLocation();
 
-        cityFinder.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dialogBuilder = new AlertDialog.Builder(getContext());
-                View popupView = getLayoutInflater().inflate(R.layout.city_search_popup, null);
-                dialogBuilder.setView(popupView);
+        cityFinder.setOnClickListener(v -> {
+            dialogBuilder = new AlertDialog.Builder(getContext());
+            View popupView = getLayoutInflater().inflate(R.layout.city_search_popup, null);
+            dialogBuilder.setView(popupView);
 
-                ImageButton closeButton = popupView.findViewById(R.id.cityCloseButton);
-                EditText changeCity = popupView.findViewById(R.id.changed_city_name);
-                Button changeButton = popupView.findViewById(R.id.changeButton);
+            ImageButton closeButton = popupView.findViewById(R.id.cityCloseButton);
+            EditText changeCity = popupView.findViewById(R.id.changed_city_name);
+            Button changeButton = popupView.findViewById(R.id.changeButton);
 
-                dialog = dialogBuilder.create();
-                dialog.show();
+            dialog = dialogBuilder.create();
+            dialog.show();
 
-                changeButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        String newCity = changeCity.getText().toString();
-                        getWeatherForNewCity(newCity);
-                        dialog.dismiss();
-                    }
-                });
+            changeButton.setOnClickListener(v1 -> {
+                String newCity = changeCity.getText().toString();
+                getWeatherForNewCity(newCity);
+                dialog.dismiss();
+            });
 
-                closeButton.setOnClickListener(t -> dialog.dismiss());
-            }
+            closeButton.setOnClickListener(t -> dialog.dismiss());
         });
     }
 
     private void getWeatherForNewCity(String city) {
+        weatherState.setVisibility(View.INVISIBLE);
+        temperature.setVisibility(View.INVISIBLE);
+        weatherIcon.setVisibility(View.INVISIBLE);
+        cityName.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+        weather5Days.setVisibility(View.INVISIBLE);
         RequestParams params = new RequestParams();
         params.put("q", city);
         params.put("appid", API);
         doNetworking(params);
-        doNetworking5Days(params);
     }
 
     @SuppressLint("MissingPermission")
@@ -142,27 +136,24 @@ public class WeatherFragment extends Fragment {
                 params.put("lon", longitude);
                 params.put("appid", API);
                 doNetworking(params);
-                doNetworking5Days(params);
             }
 
             @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
+            public void onStatusChanged(String provider, int status, Bundle extras) { }
 
             @Override
-            public void onProviderEnabled(@NonNull String provider) {
-
-            }
+            public void onProviderEnabled(@NonNull String provider) { }
 
             @Override
             public void onProviderDisabled(@NonNull String provider) {
-                // unable to get location
+                Toast.makeText(getContext(), "Unable to obtain current location",
+                        Toast.LENGTH_SHORT).show();
             }
         };
-        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
             //                                          int[] grantResults)
@@ -172,78 +163,72 @@ public class WeatherFragment extends Fragment {
             return;
         }
         locationManager.requestLocationUpdates(Location_Provider, MIN_TIME, MIN_DISTANCE, locationListener);
-
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(getContext(), "Location Get Successfully", Toast.LENGTH_SHORT).show();
-
-            }
-        }
     }
 
     private void doNetworking(RequestParams params) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-
+        AsyncHttpClient client1 = new AsyncHttpClient();
+        client1.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
+                try {
+                    String city = response.getString("name");
+                    int condition = response.getJSONArray("weather").getJSONObject(0).getInt("id");
+                    String weatherType = response.getJSONArray("weather").getJSONObject(0).getString("main");
+                    String icon = updateWeatherIcon(condition);
+                    double temp = response.getJSONObject("main").getDouble("temp") - 273.15;
+                    int roundedValue = (int) Math.rint(temp);
+                    String t = Integer.toString(roundedValue);
+                    temperature.setText(String.format("%s°C", t));
+                    cityName.setText(city);
+                    weatherState.setText(weatherType);
+                    int resourceID = getResources().getIdentifier(icon, "drawable", getActivity().getPackageName());
+                    weatherIcon.setImageResource(resourceID);
 
-                WeatherData weatherD = WeatherData.fromJson(response);
-                updateUI(weatherD);
-            }
+                    RequestParams params = new RequestParams();
+                    params.put("lat", response.getJSONObject("coord").getString("lat"));
+                    params.put("lon", response.getJSONObject("coord").getString("lon"));
+                    params.put("appid", API);
+                    client1.get(WEATHER_URL_5_DAYS, params, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+                            List<WeatherItem> forecast = getForecast(response);
+                            WeatherAdapter weatherAdapter = new WeatherAdapter(getContext(), forecast);
+                            weather5Days.setHasFixedSize(true);
+                            weather5Days.setLayoutManager(new LinearLayoutManager(getContext()));
+                            weather5Days.setAdapter(weatherAdapter);
+                            super.onSuccess(statusCode, headers, response);
+                            temperature.setVisibility(View.VISIBLE);
+                            cityFinder.setVisibility(View.VISIBLE);
+                            cityName.setVisibility(View.VISIBLE);
+                            weatherState.setVisibility(View.VISIBLE);
+                            weatherIcon.setVisibility(View.VISIBLE);
+                            weather5Days.setVisibility(View.VISIBLE);
+                            progressBar.setVisibility(View.GONE);
+                        }
 
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-//                super.onFailure(statusCode, headers, throwable, errorResponse);
-            }
-        });
-    }
-
-    private void doNetworking5Days(RequestParams params) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get(WEATHER_URL_5_DAYS, params, new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
-
-                WeatherData5Days weatherD = WeatherData5Days.fromJson(response);
-                updateUI(weatherD);
-//                super.onSuccess(statusCode, headers, response);
+                        @Override
+                        public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                            super.onFailure(statusCode, headers, throwable, errorResponse);
+                            Toast.makeText(getContext(), "Unable to obtain weather data",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getContext(), "Unable to obtain weather data",
+                            Toast.LENGTH_SHORT).show();
+                }
             }
 
             @Override
             public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable throwable, JSONObject errorResponse) {
-//                super.onFailure(statusCode, headers, throwable, errorResponse);
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+                Toast.makeText(getContext(), "Unable to obtain weather data",
+                        Toast.LENGTH_SHORT).show();
+                progressBar.setVisibility(View.INVISIBLE);
             }
         });
-    }
-
-    private void updateUI(WeatherData weatherData) {
-        temperature.setText(weatherData.getTemperature());
-        cityName.setText(weatherData.getCity());
-        weatherState.setText(weatherData.getWeatherType());
-        int resourceID = getResources().getIdentifier(weatherData.getIcon(), "drawable", getActivity().getPackageName());
-        weatherIcon.setImageResource(resourceID);
-
-        temperature.setVisibility(View.VISIBLE);
-        cityFinder.setVisibility(View.VISIBLE);
-        cityName.setVisibility(View.VISIBLE);
-        weatherState.setVisibility(View.VISIBLE);
-        weatherIcon.setVisibility(View.VISIBLE);
-        progressBar.setVisibility(View.GONE);
-    }
-
-    private void updateUI(WeatherData5Days weatherData) {
-        weatherIconsA = new String[] { weatherData.getIconDay1(), weatherData.getIconDay2(), weatherData.getIconDay3(), weatherData.getIconDay4(), weatherData.getIconDay5() };
-        temperatureA = new String[] { weatherData.getTemperatureDay1(), weatherData.getTemperatureDay2(), weatherData.getTemperatureDay3(), weatherData.getTemperatureDay4(), weatherData.getTemperatureDay5()};
-        datesA = new Date[] { weatherData.getDay1(), weatherData.getDay2(), weatherData.getDay3(), weatherData.getDay4(), weatherData.getDay5() };
-        WeatherAdapter weatherAdapter = new WeatherAdapter(WeatherFragment.this, weatherIconsA, datesA, temperatureA);
-        weather5Days.setAdapter(weatherAdapter);
     }
 
     @Override
@@ -251,6 +236,57 @@ public class WeatherFragment extends Fragment {
         super.onPause();
         if(locationManager != null) {
             locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    private static String updateWeatherIcon(int condition) {
+        if (condition >= 0 && condition <= 300 || condition >= 900 && condition <= 902 || condition >= 905 && condition <= 1000) {
+            return "weather_thunder";
+        } else if (condition >= 300 && condition <= 500) {
+            return "weather_light_rain";
+        } else if (condition >= 500 && condition <= 600) {
+            return "weather_heavy_rain";
+        } else if (condition >= 600 && condition <= 700) {
+            return "weather_snow";
+        } else if (condition >= 701 && condition <= 771) {
+            return "weather_fog";
+        } else if (condition >= 772 && condition < 800 || condition >= 801 && condition <= 804) {
+            return "weather_cloudy";
+        } else if (condition == 800 || condition == 904) {
+            return "weather_sunny";
+        } else if (condition == 903) {
+            return "weather_snow";
+        } else {
+            return "No matching weather";
+        }
+    }
+
+    private static String timestampToDate(long timestamp) {
+        Date date = new Date(timestamp * 1000);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat formatter = new SimpleDateFormat("E, dd MMM yyyy");
+        return formatter.format(date);
+    }
+
+    public List<WeatherItem> getForecast(JSONObject jsonObject) {
+        try {
+            List<WeatherItem> forecast = new ArrayList<>();
+            Log.d("Track", "retrieving info");
+            for (int i = 0; i < 5; i++) {
+                JSONObject weather = jsonObject.getJSONArray("daily").getJSONObject(i);
+                String date = timestampToDate(weather.getLong("dt"));
+                int condition = weather.getJSONArray("weather").getJSONObject(0).getInt("id");
+                String weatherType = weather.getJSONArray("weather").getJSONObject(0).getString("main");
+                String icon = updateWeatherIcon(condition);
+                double temp = weather.getJSONObject("temp").getDouble("day") - 273.15;
+                int roundedValue = (int) Math.rint(temp);
+                forecast.add(new WeatherItem(icon, Integer.toString(roundedValue), weatherType, condition, date));
+            }
+            return forecast;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Toast.makeText(getContext(), "Unable to obtain weather data",
+                    Toast.LENGTH_SHORT).show();
+            return null;
         }
     }
 }
